507479e364c50d228a4856450ef1ce5938975a01
UMEZAWA Takeshi <umezawa@utsoft.org>
2020-04-05 23:40:11 +0900

    Refactor generation functions again.

--- gen_asm.pl
+++ gen_asm.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 
-my $NREP = 4800;
+my $NREP = 240;
 my $NLOOP = 10000;
 
 print <<EOT;
@@ -38,7 +38,7 @@ sub epilogue() {
 	)"
 		: /* no output */
 		: /* no input */
-		: "rax", "rbx", "rcx", "rdx", "r8", "r9", "r10", "r11", "r12", "r13", "r14", "r15");
+		: "rax", "rbx", "rcx", "rdx", "rsi", "rdi", "r8", "r9", "r10", "r11", "r12", "r13", "r14", "r15");
 
 	read_cycle_counter(stop);
 	tc->tsc = stop - start;
@@ -62,233 +62,99 @@ EOT
 	epilogue();
 }
 
+
+sub gen_regs($) {
+	my @ret = ();
+
+	for (my $i = 0; $i < 10; ++$i) {
+		push(@ret, "$_[0]$i");
+	}
+	for (my $i = 13; $i <= 15; ++$i) {
+		push(@ret, "$_[0]$i");
+	}
+
+	return \@ret;
+}
+
+my $r64 = [ "rax", "rbx", "rdx", "rsi", "rdi", "r8", "r9", "r10", "r11", "r12", "r13", "r14", "r15" ];
+my $xmm = gen_regs("xmm");
+my $ymm = gen_regs("ymm");
+my $zmm = gen_regs("zmm");
+
+
 # 生成するperl関数の命名規則は以下の通り
-# gen_drr
+# gen_n3_c3
 # d : dst レジスタに依存関係が発生する命令。 add など
 # n : dst レジスタに依存関係が発生しない命令。ほぼすべての vex prefix 命令や PMOVZXxx など
-# rr : 汎用レジスタ2つ
-# xx : xmm レジスタ2つ
+# 3 : オペランドが3つ
+# c3 : 3番目のオペランドに定数を指定（BSR や PEXT など値によって速度が変わる命令で使う）
 #
 # 生成されるC++関数の命名規則は以下の通り
 # add_r64_tp
 # tp : スループット計測
 # lt1 : 最初の依存関係オペランドからのレイテンシ
 # lt2 : 2番目の依存関係オペランドからのレイテンシ
-# _2i : 2番目の依存関係オペランドに整数の定数を渡す
 
-################ gen_drr ################
 
-sub gen_drr($$) {
-	my($inst, $label) = @_;
+################ gen_d2 ################
 
-	gen("$_[1]_tp", 10, <<EOT);
-	$inst	rax, r15
-	$inst	rbx, r15
-	$inst	rdx, r15
-	$inst	r8,  r15
-	$inst	r9,  r15
-	$inst	r10, r15
-	$inst	r11, r15
-	$inst	r12, r15
-	$inst	r13, r15
-	$inst	r14, r15
-EOT
+sub gen_d2($$$$) {
+	my($inst, $label, $o1, $o2) = @_;
 
-	gen("$_[1]_lt1", 1, <<EOT);
-	$inst	rax, r15
-EOT
-
-	gen("$_[1]_lt2", 10, <<EOT);
-	$inst	rbx, rax
-	$inst	rdx, rbx
-	$inst	r8,  rdx
-	$inst	r9,  r8
-	$inst	r10, r9
-	$inst	r11, r10
-	$inst	r12, r11
-	$inst	r13, r12
-	$inst	r14, r13
-	$inst	rax, r14
-EOT
+	gen("$_[1]_tp", 10, join("\n", map { "$inst $o1->[$_], $o2->[-1]" } 0..9));
+	gen("$_[1]_lt1", 1, "$inst $o1->[0], $o2->[-1]");
+	gen("$_[1]_lt2", 10, join("\n", map { "$inst $o1->[($_+1)%10], $o2->[$_]" } 0..9));
 }
 
-################ gen_nrr ################
+################ gen_n2 ################
 
-sub gen_nrr($$) {
-	my($inst, $label) = @_;
+sub gen_n2($$$$) {
+	my($inst, $label, $o1, $o2) = @_;
 
-	gen("$_[1]_tp", 1, <<EOT);
-	$inst	rax, r15
-EOT
-
-	gen("$_[1]_lt1", 1, <<EOT);
-	print <<EOT;
-	$inst	rax, rax
-EOT
+	gen("$_[1]_tp", 1, "$inst $o1->[0], $o2->[-1]");
+	gen("$_[1]_lt1", 1, "$inst $o1->[0], $o2->[0]");
 }
 
-################ gen_nrrr_2i ################
+################ gen_n3 ################
 
-sub gen_nrrr_2i($$$) {
-	my($inst, $label, $imm2) = @_;
+sub gen_n3($$$$$) {
+	my($inst, $label, $o1, $o2, $o3) = @_;
 
-	gen("$_[1]_tp", 1, <<EOT, <<EOPRE);
-	$inst	rax, r14, r15
-EOT
-	mov	r15, $imm2
-EOPRE
-
-	gen("$_[1]_lt1", 1, <<EOT, <<EOPRE);
-	$inst	rax, rax, r15
-EOT
-	mov	r15, $imm2
-EOPRE
+	gen("$_[1]_tp", 1, "$inst $o1->[0], $o2->[-2], $o3->[-1]");
+	gen("$_[1]_lt1", 1, "$inst $o1->[0], $o2->[0], $o3->[-1]");
+	gen("$_[1]_lt2", 1, "$inst $o1->[0], $o2->[-1], $o3->[0]");
 }
 
-################ gen_nxx ################
+################ gen_n4 ################
 
-sub gen_nxx($$) {
-	my($inst, $label) = @_;
+sub gen_n4($$$$$$) {
+	my($inst, $label, $o1, $o2, $o3, $o4) = @_;
 
-	gen("$_[1]_tp", 1, <<EOT);
-	$inst	xmm0, xmm15
-EOT
-
-	gen("$_[1]_lt1", 1, <<EOT);
-	$inst	xmm0, xmm0
-EOT
+	gen("$_[1]_tp", 1, "$inst $o1->[0], $o2->[-3], $o3->[-2], $o4->[-1]");
+	gen("$_[1]_lt1", 1, "$inst $o1->[0], $o2->[0], $o3->[-2], $o4->[-1]");
+	gen("$_[1]_lt2", 1, "$inst $o1->[0], $o2->[-2], $o3->[0], $o4->[-1]");
+	gen("$_[1]_lt3", 1, "$inst $o1->[0], $o2->[-2], $o3->[-1], $o4->[0]");
 }
 
-################ gen_dxx ################
+################ gen_n3_c3 ################
 
-sub gen_dxx($$) {
-	my($inst, $label) = @_;
+sub gen_n3_c3($$$$$$) {
+	my($inst, $label, $o1, $o2, $o3, $c3) = @_;
 
-	gen("$_[1]_tp", 10, <<EOT);
-	$inst	xmm0, xmm15
-	$inst	xmm1, xmm15
-	$inst	xmm2, xmm15
-	$inst	xmm3, xmm15
-	$inst	xmm4, xmm15
-	$inst	xmm5, xmm15
-	$inst	xmm6, xmm15
-	$inst	xmm7, xmm15
-	$inst	xmm8, xmm15
-	$inst	xmm9, xmm15
-EOT
-
-	gen("$_[1]_lt1", 1, <<EOT);
-	$inst	xmm0, xmm15
-EOT
-
-	gen("$_[1]_lt2", 10, <<EOT);
-	$inst	xmm1, xmm0
-	$inst	xmm2, xmm1
-	$inst	xmm3, xmm2
-	$inst	xmm4, xmm3
-	$inst	xmm5, xmm4
-	$inst	xmm6, xmm5
-	$inst	xmm7, xmm6
-	$inst	xmm8, xmm7
-	$inst	xmm9, xmm8
-	$inst	xmm0, xmm9
-EOT
+	my $pre = "mov $o3->[-1], $c3";
+	gen("$_[1]_tp", 1, "$inst $o1->[0], $o2->[-2], $o3->[-1]", $pre);
+	gen("$_[1]_lt1", 1, "$inst $o1->[0], $o2->[0], $o3->[-1]", $pre);
 }
 
-################ gen_nxxx ################
 
-sub gen_nxxx($$) {
-	my($inst, $label) = @_;
-
-	gen("$_[1]_tp", 1, <<EOT);
-	$inst	xmm0, xmm14, xmm15
-EOT
-
-	gen("$_[1]_lt1", 1, <<EOT);
-	$inst	xmm0, xmm0, xmm15
-EOT
-
-	gen("$_[1]_lt2", 1, <<EOT);
-	$inst	xmm0, xmm15, xmm0
-EOT
-}
-
-################ gen_nxxxx ################
-
-sub gen_nxxxx($$) {
-	my($inst, $label) = @_;
-
-	gen("$_[1]_tp", 1, <<EOT);
-	$inst	xmm0, xmm13, xmm14, xmm15
-EOT
-
-	gen("$_[1]_lt1", 1, <<EOT);
-	$inst	xmm0, xmm0, xmm14, xmm15
-EOT
-
-	gen("$_[1]_lt2", 1, <<EOT);
-	$inst	xmm0, xmm14, xmm0, xmm15
-EOT
-
-	gen("$_[1]_lt3", 1, <<EOT);
-	$inst	xmm0, xmm14, xmm15, xmm0
-EOT
-}
-
-################ gen_nyy ################
-
-sub gen_nyy($$) {
-	my($inst, $label) = @_;
-
-	gen("$_[1]_tp", 1, <<EOT);
-	$inst	ymm0, ymm15
-EOT
-
-	gen("$_[1]_lt1", 1, <<EOT);
-	$inst	ymm0, ymm0
-EOT
-}
-
-################ gen_nyx ################
-
-sub gen_nyx($$) {
-	my($inst, $label) = @_;
-
-	gen("$_[1]_tp", 1, <<EOT);
-	$inst	ymm0, xmm15
-EOT
-
-	gen("$_[1]_lt1", 1, <<EOT);
-	$inst	ymm0, xmm0
-EOT
-}
-
-################ gen_nyyy ################
-
-sub gen_nyyy($$) {
-	my($inst, $label) = @_;
-
-	gen("$_[1]_tp", 1, <<EOT);
-	$inst	ymm0, ymm14, ymm15
-EOT
-
-	gen("$_[1]_lt1", 1, <<EOT);
-	$inst	ymm0, ymm0, ymm15
-EOT
-
-	gen("$_[1]_lt2", 1, <<EOT);
-	$inst	ymm0, ymm15, ymm0
-EOT
-}
-
-gen_drr("add", "add_r64");
-gen_dxx("paddb", "paddb_xmm");
-gen_nxxx("vpaddb", "vpaddb_xmm");
-gen_nxx("pmovzxbw", "pmovzxbw_xmm");
-gen_nyx("vpmovzxbw", "vpmovzxbw_ymm");
-gen_nxxxx("vpblendvb", "vpblendvb_xmm");
-gen_nrrr_2i("pext", "pext_all0", "0x0000000000000000");
-gen_nrrr_2i("pext", "pext_all1", "0xffffffffffffffff");
-gen_nrrr_2i("pext", "pext_half", "0x5555555555555555");
-gen_nrrr_2i("pext", "pext_lo",   "0x00000000ffffffff");
-gen_nrrr_2i("pext", "pext_hi",   "0xffffffff00000000");
+gen_d2("add", "add_r64", $r64, $r64);
+gen_d2("paddb", "paddb_xmm", $xmm, $xmm);
+gen_n3("vpaddb", "vpaddb_xmm", $xmm, $xmm, $xmm);
+gen_n2("pmovzxbw", "pmovzxbw_xmm", $xmm, $xmm);
+gen_n2("vpmovzxbw", "vpmovzxbw_ymm", $ymm, $xmm);
+gen_n4("vpblendvb", "vpblendvb_xmm", $xmm, $xmm, $xmm, $xmm);
+gen_n3_c3("pext", "pext_all0", $r64, $r64, $r64, "0x0000000000000000");
+gen_n3_c3("pext", "pext_all1", $r64, $r64, $r64, "0xffffffffffffffff");
+gen_n3_c3("pext", "pext_half", $r64, $r64, $r64, "0x5555555555555555");
+gen_n3_c3("pext", "pext_lo",   $r64, $r64, $r64, "0x00000000ffffffff");
+gen_n3_c3("pext", "pext_hi",   $r64, $r64, $r64, "0xffffffff00000000");
